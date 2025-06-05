771341044f669d8a8fa5735d5f5d6a62d985a86f
LucaIapadre98 <luca.iap.1998@gmail.com>
2025-06-05 15:34:43 +0200

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header(){
+    return (
+        <div className="title">
+            <h1>BoolFlix</h1>
+        </div>
+    )
+}
---
+++ src/components/Main.jsx
@@ -0,0 +1,26 @@
+import { useEffect  } from "react";
+import axios from "axios";
+
+export default function Main(){
+
+    const apiUrl="https://api.themoviedb.org/3"
+    const apiKey= "63f98dc4b840e55db5609ed318833f2b";
+    const query="Harry";
+    const apiParams= {api_key: apiKey, query};
+
+    const queryString = new URLSearchParams(apiParams).toString();
+    console.log(queryString);
+    
+
+    useEffect(() =>{
+        axios.get(`${apiUrl}/search/movie?${queryString}`)
+    })
+    return (
+        <div className="container-form">
+            <form className="d-flex" role="search">
+                <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+                <button className="btn btn-outline-success" type="submit">Button</button>
+            </form>
+        </div>
+    )
+}
