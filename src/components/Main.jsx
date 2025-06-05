8b6e43ef7d6dfe3a555eab146ee7322cca765c63
LucaIapadre98 <luca.iap.1998@gmail.com>
2025-06-05 17:11:26 +0200

    fatta la chimata con axios

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,25 +1,56 @@
-import { useEffect  } from "react";
 import axios from "axios";
+import { useEffect, useState } from "react";
 
 export default function Main(){
-
+    const [characters, setCharacters] = useState([]);
     const apiUrl="https://api.themoviedb.org/3"
     const apiKey= "63f98dc4b840e55db5609ed318833f2b";
     const query="Harry";
-    const apiParams= {api_key: apiKey, query};
 
-    const queryString = new URLSearchParams(apiParams).toString();
-    console.log(queryString);
-    
-    useEffect(() =>{
-        axios.get(`${apiUrl}/search/movie?${queryString}`)
-    })
+    const fetchCharacters = () => {
+        axios.get(`${apiUrl}/search/movie?api_key=${apiKey}&query=${query}`).then((res)=>{
+            const{ results } = res.data;
+
+            const normalizedcharacters = results.map((character)=>{
+                const { original_language, original_title, title, vote_average} = character;
+                return { original_language, original_title, title, vote_average};
+            });
+
+            setCharacters(normalizedcharacters);
+        });
+    }
+    useEffect(fetchCharacters, []);
+
     return (
-        <div className="container-form">
-            <form className="d-flex" role="search">
-                <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-                <button className="btn btn-outline-success" type="submit">Button</button>
-            </form>
-        </div>
-    );
+        <>
+            <div className="container-form">
+                <form className="d-flex" role="search">
+                    <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+                    <button className="btn btn-outline-success" type="submit">Button</button>
+                </form>
+            </div>
+            <div className="container-card">
+                {characters ? (
+                    <div className="card">
+                        {characters.map((character) =>(
+                            <>
+                                <div className="card-header" key={character.id}>
+                                    <h3>{character.title}</h3>
+                                    <h6>{character.original_title}</h6>
+                                </div>
+                                <div className="card-body">
+                                    <span>{character.original_language}</span>
+                                    <p>{character.vote_average}</p>
+                                </div>
+                            </>
+                        ))}
+                        
+                    </div>
+                ) : (
+                    <h3>Loading...</h3>
+                )}
+               
+            </div>
+        </>
+    )
 }
